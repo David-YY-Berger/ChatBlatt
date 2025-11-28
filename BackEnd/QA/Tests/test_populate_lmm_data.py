c87d42cb3bce2bf6eb13e63a237c2ed95d32f9b7
davidberger <davidberger2448@gmail.com>
2025-11-28 14:15:07 +0200

    added simple LMM test

---
+++ BackEnd/QA/Tests/test_populate_lmm_data.py
@@ -0,0 +1,17 @@
+# bs'd
+from BackEnd.DataPipeline.DBParentClass import DBParentClass
+from BackEnd.DataPipeline.LMM_api.GeminiLmmCaller import GeminiLmmCaller
+
+
+class TestsForPopulateLmmData(DBParentClass):
+
+    def setUp(self):
+        super().setUp()
+        self.lmm_caller = GeminiLmmCaller()
+
+    def tearDown(self):
+        pass
+
+    def test_simple_lmm_call(self):
+        res = self.lmm_caller.call("foo")
+        assert (res.content.__contains__("foo"))
