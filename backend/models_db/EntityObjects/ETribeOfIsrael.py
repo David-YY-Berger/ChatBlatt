23858230bca4cf12e0fd156565b40e2026bd6d53
davidberger <davidberger2448@gmail.com>
2026-05-13 21:33:38 +0300

    d

--- backend/models_db/EntityObjects/ETribeOfIsrael.py
+++ backend/models_db/EntityObjects/ETribeOfIsrael.py
@@ -1,10 +1,15 @@
 # bs"d - lehagdil torah velahadir
 
-from backend.models_db.EntityObjects.Entity import Entity
+from typing import List
+from backend.models_db.EntityObjects.Entity import Entity, TransientField
 from backend.models_db.Enums import EntityType
 
 
 class ETribeOfIsrael(Entity):
     entityType: EntityType = EntityType.ETribeOfIsrael
+
+    # transient fields - TribeOfIsrael → Person/Group
+    membersOfTribeIsrael: List[str] = TransientField(default_factory=list)
+
 
 # DEFINED [13 tribes of Israel]
