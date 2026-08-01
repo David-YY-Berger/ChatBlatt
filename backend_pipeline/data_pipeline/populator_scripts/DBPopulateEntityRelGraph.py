914dc0e01d9e54ba1ee9d5ce74fb63b9838f0b52
davidberger <davidberger2448@gmail.com>
2026-08-01 22:08:04 +0300

    clean bad code

--- backend_pipeline/data_pipeline/populator_scripts/DBPopulateEntityRelGraph.py
+++ backend_pipeline/data_pipeline/populator_scripts/DBPopulateEntityRelGraph.py
@@ -392,38 +392,6 @@ class DBPopulateEntityRelGraph(DBPopulateLlmBase):
 
         return None
 
-    ############################################## Populating Metadata ###############################################
-
-    async def test_populate_source_meta_data(self):
-
-        print("Starting to populate source meta data.")
-        all_srcs = self.db_api.get_all_src_contents_by_book(Books.GENESIS)
-        # src_processed = 0
-        # num_srcs_to_process = 20
-        #
-        # for src in all_srcs:
-        #     if src_processed >= num_srcs_to_process:
-        #         break
-        #
-        #     if self.db_api.is_src_metadata_exist(src.key):
-        #         continue
-        #
-        #     analyzed_response = self.lmm_caller.analyze_src(src.content[SourceContentType.EN])
-        #     src_metadata = SourceMetadata(key = src.key, source_type=src.source_type)
-        #     src_metadata.passage_types = analyzed_response.e_passage_types
-        #     src_metadata.summary_en = analyzed_response.summary_en
-        #     src_metadata.summary_heb = analyzed_response.summary_heb
-        #
-        #     entity_keys = self.entity_rel_mngr.insert_entity_map(analyzed_response.get_entity_map(), src.get_src_type())
-        #     src_metadata.entities.update(entity_keys)
-        #
-        #     rel_keys = self.entity_rel_mngr.insert_rel_map(analyzed_response.get_all_relationships(), src.get_src_type())
-        #     src_metadata.entities.update(rel_keys)
-        #
-        #     is_success = self.db_api.insert_or_update_source_metadata(src)
-        #     if is_success:
-        #         src_processed += 1
-        return
 
     ############################################## helper methods ####################################################
 
